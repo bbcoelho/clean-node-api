793400ea079f95b873a820747b8900467151e7ea
bbcoelho <bbcoelho@gmail.com>
2022-07-07 16:08:13 -0300

    test: ensure SignUpController calls EmailValidator with a valid email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -110,4 +110,24 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new InvalidParamError('email'))
     })
+
+    test('Should call EmailValidator with a valid email', () => {
+
+        const { sut, emailValidatorStub } = makeSut()
+
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'valid_email@gmail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        
+        sut.handle(httpRequest)
+
+        expect(isValidSpy).toHaveBeenCalledWith('valid_email@gmail.com')
+    })
 })
