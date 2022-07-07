ec7f665b3835aaf7118e0f3763b859f93391a3b7
bbcoelho <bbcoelho@gmail.com>
2022-07-07 11:19:01 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,7 +2,9 @@ import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
+
     test('Should return 400 if no name is provided', () => {
+
         const sut = new SignUpController()
         const httpRequest = {
             body: {
@@ -12,11 +14,13 @@ describe('SignUp Controller', () => {
             }
         }
         const httpResponse = sut.handle(httpRequest)
+
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('name'))
     })
 
     test('Should return 400 if no email is provided', () => {
+
         const sut = new SignUpController()
         const httpRequest = {
             body: {
@@ -26,6 +30,7 @@ describe('SignUp Controller', () => {
             }
         }
         const httpResponse = sut.handle(httpRequest)
+        
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
