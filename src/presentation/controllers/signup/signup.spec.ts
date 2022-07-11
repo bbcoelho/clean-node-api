e975bd525dcc0a44b6facaaeed5085fcf1aa99ae
bbcoelho <bbcoelho@gmail.com>
2022-07-11 15:52:10 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -246,4 +246,28 @@ describe('SignUp Controller', () => {
         })
     })
 
+    test('Should return 200 if valid data is provided', () => {
+
+        const { sut } = makeSut()
+
+        const httpRequest = {
+            body: {
+                name: 'valid_name',
+                email: 'valid_email@gmail.com',
+                password: 'valid_password',
+                passwordConfirmation: 'valid_password'
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@gmail.com',
+            password: 'valid_password',
+        })
+    })
+    
 })
